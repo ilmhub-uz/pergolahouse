a16aaf482562ff670550394ff16b0a53d15b2b97
Rizo11 <muhammadrizomr1110@gmail.com>
2022-02-02 16:18:24 +0500

    categories done 2

---
+++ webapp/Models/SingleCategoryItem.cs
@@ -0,0 +1,10 @@
+namespace webapp.Models;
+
+public class SingleCategoryItem
+{
+    public string Title { get; set; }
+    
+    public string IconImage { get; set; }
+    
+    public int TotalItems { get; set; }
+}
