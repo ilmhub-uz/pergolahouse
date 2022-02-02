c2112e92a4c1adc15b8a104b7a36958fe90db20a
Shukhrat Utaboev <89694184+shukhratutaboev@users.noreply.github.com>
2022-02-02 18:34:51 +0500

    Revert "Shukhrat/2"

--- webapp/wwwroot/assets/js/main.js
+++ webapp/wwwroot/assets/js/main.js
@@ -69,18 +69,6 @@
                }
            }
        });
-
-       var path = window.location.pathname.split('/');
-        var base = document.getElementsByTagName('base')[0];
-        if (window.location.host.includes('localhost')) {
-            base.setAttribute('href', '/');
-        } else if (path.length > 2) {
-            base.setAttribute('href', '/' + path[1] + '/');
-        } else if (path[path.length - 1].length != 0) {
-            window.location.replace(window.location.origin + window.location.pathname + '/' + window.location.search);
-        }
-
-
     }
 })();
 
