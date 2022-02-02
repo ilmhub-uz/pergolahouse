712452a29f04b3ab8ade82a061bfe464750e49fa
shukhratutaboev <89694184+shukhratutaboev@users.noreply.github.com>
2022-02-02 13:25:13 +0000

    Deploying to gh-pages from @ ilmhub-uz/pergolahouse@d667568681844c910e7bffedcd3e4f54c2a7622e 🚀

--- assets/js/main.js
+++ assets/js/main.js
@@ -69,6 +69,18 @@
                }
            }
        });
+
+       var path = window.location.pathname.split('/');
+        var base = document.getElementsByTagName('base')[0];
+        if (window.location.host.includes('localhost')) {
+            base.setAttribute('href', '/');
+        } else if (path.length > 2) {
+            base.setAttribute('href', '/' + path[1] + '/');
+        } else if (path[path.length - 1].length != 0) {
+            window.location.replace(window.location.origin + window.location.pathname + '/' + window.location.search);
+        }
+
+
     }
 })();
 
