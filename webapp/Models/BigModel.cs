c3e102d6009fade2e1e3e665b0b952b995facdd5
abubakr0904 <b1903874@mdis.uz>
2022-02-04 20:45:47 +0500

    by category sort is working itemsgrid component

---
+++ webapp/Models/BigModel.cs
@@ -0,0 +1,10 @@
+namespace webapp.Models;
+
+public class BigModel
+{
+    public string Tag { get; set; }
+    
+    public string Category { get; set; }
+    
+    public ItemModel Model { get; set; }
+}
