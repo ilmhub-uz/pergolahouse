99f3304792566fa0c8bda245652b4dc345566ad5
BobAzizbek <boboyevazizbek2006@gmail.com>
2022-02-09 18:57:13 +0500

    removed useless code in program.cs

--- webapp/Program.cs
+++ webapp/Program.cs
@@ -1,8 +1,6 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Microsoft.JSInterop;
 using webapp;
-using webapp.Models;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
