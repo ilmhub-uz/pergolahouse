ecaabfa9a00961f0e259ef3187f1812707bdfd3c
abubakr0904 <b1903874@mdis.uz>
2022-02-10 10:16:46 +0500

    merge conflict resolving

---
+++ webapp/Pages/Index.razor.cs
@@ -0,0 +1,23 @@
+﻿using System.Net.Http.Json;
+using Microsoft.AspNetCore.Components;
+using webapp.Models;
+
+namespace webapp.Pages;
+
+public partial class Index : ComponentBase
+{
+    public List<ItemModel> ItemModels { get; set; } = new List<ItemModel>();
+
+    [Inject]
+    public HttpClient Http { get; set; }
+
+    public List<string> Categories { get; set; } = new List<string>();
+
+
+
+    protected override async Task OnInitializedAsync()
+    {
+        ItemModels = await Http.GetFromJsonAsync<List<ItemModel>>("data/data.json");
+        StateHasChanged();
+    }
+} 
