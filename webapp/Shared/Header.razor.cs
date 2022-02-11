5b60e8a317c0ae8188a484ccc335aefac39dd65c
abubakr0904 <b1903874@mdis.uz>
2022-02-11 14:42:29 +0500

    category fixing

--- webapp/Shared/Header.razor.cs
+++ webapp/Shared/Header.razor.cs
@@ -7,7 +7,8 @@ public partial class Header : ComponentBase
 {
     [Inject]
     public NavigationManager NavigationManager { get; set; }
-    
+
+    public bool VisibilityStatus { get; set; } = false;
     
     [Parameter]
     public List<string> Categories { get; set; } = new List<string>();
@@ -15,5 +16,10 @@ public partial class Header : ComponentBase
     public void Navigate(string category)
     {
         NavigationManager.NavigateTo("/categories/" + category, forceLoad:true);
+    }
+
+    public void ChangeVisibilityStatus()
+    {
+        VisibilityStatus = VisibilityStatus ? false : true;
     }
 } 
